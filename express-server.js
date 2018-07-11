a79a2446fd2e9968e78cb5fcb418796f3cf15fe1
Ryan White <ryanjwhite10@gmail.com>
2018-07-11 00:07:41 +0000

    Added GET and POST routes, and function to generate random 6 character string

--- express-server.js
+++ express-server.js
@@ -1,13 +1,28 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 const PORT = 8080;
 
 app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 const urlDatabase = {
   'b2xVn2': 'http://www.lighthouselabs.ca',
   '9sm5xK': 'http://www.google.com'
 };
+
+function generateRandomString() {
+  const alphanumericChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890';
+  let randomString = '';
+
+  for (let i = 0; i < 6; i++) {
+    let random = Math.floor(Math.random() * 63);
+    randomString += alphanumericChars[random];
+  }
+
+  return randomString;
+}
 
 app.get('/', (req, res) => {
   res.end('Hello!');
@@ -16,6 +31,15 @@ app.get('/', (req, res) => {
 app.get('/urls', (req, res) => {
   let templateVars = { urls: urlDatabase };
   res.render('urls_index', templateVars);
+});
+
+app.get('/urls/new', (req, res) => {
+  res.render('urls_new');
+});
+
+app.post('/urls', (req, res) => {
+  console.log(req.body);
+  res.send('OK');
 });
 
 app.get('/urls/:id', (req, res) => {
